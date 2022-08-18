8874dc9676051f0e0ed754ecad10db2e94db1147
Andre Vianna <andre.vianna@tc.gc.ca>
2022-08-18 02:09:17 -0400

    Correct indirection

--- Sandbox.Core/Results/FailureResult.cs
+++ Sandbox.Core/Results/FailureResult.cs
@@ -1,20 +1,3 @@
-﻿#nullable disable
+﻿namespace System.Results;
 
-namespace System.Results;
-
-public abstract record FailureResult : IResult {
-}
-
-public abstract record FailureResultWithErrors : FailureResult, IResultWithErrors {
-    protected FailureResultWithErrors(IEnumerable<Error> errors) {
-        Errors = errors?.ToArray() ?? throw new ArgumentNullException(nameof(errors));
-        if (Errors.Count == 0) throw new ArgumentException("Collection cannot be empty.", nameof(errors));
-        if (Errors.Any(e => e is null)) throw new ArgumentException("Collection cannot contain a null element", nameof(errors));
-    }
-
-    protected FailureResultWithErrors(Error error)
-        : this(new[] { error ?? throw new ArgumentNullException(nameof(error)) }) {
-    }
-
-    public IReadOnlyCollection<Error> Errors { get; }
-}
+public abstract record FailureResult : IResult;
--- Sandbox.Core/Results/ValidationErrorResult.cs
+++ Sandbox.Core/Results/ValidationErrorResult.cs
@@ -1,8 +1,19 @@
-﻿namespace System.Results;
+﻿#nullable disable
 
-public record ValidationErrorResult : FailureResultWithErrors {
-    public ValidationErrorResult(IEnumerable<Error> errors) : base(errors) { }
-    public ValidationErrorResult(Error error) : base(error) { }
+namespace System.Results;
+
+public record ValidationErrorResult : FailureResult {
+    public ValidationErrorResult(IEnumerable<Error> errors) {
+        Errors = errors?.ToArray() ?? throw new ArgumentNullException(nameof(errors));
+        if (Errors.Count == 0) throw new ArgumentException("Collection cannot be empty.", nameof(errors));
+        if (Errors.Any(e => e is null)) throw new ArgumentException("Collection cannot contain a null element.", nameof(errors));
+    }
+
+    public ValidationErrorResult(Error error)
+        : this(new[] { error ?? throw new ArgumentNullException(nameof(error)) }) {
+    }
+
+    public IReadOnlyCollection<Error> Errors { get; }
 }
 
 public record ValidationErrorResult<TValue> : ValidationErrorResult, IResult<TValue> {
