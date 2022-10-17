e53f1d9d2e7e2275c796a8d7c415a4a81c52a576
Andre Vianna <andre.vianna@tc.gc.ca>
2022-10-17 09:44:18 -0400

    Add foreach on range

---
+++ Sandbox.System.UnitTests/CustomRangeEnumeratorTests.cs
@@ -0,0 +1,19 @@
+﻿namespace System;
+
+public class CustomRangeEnumeratorTests {
+    [Fact]
+    public void CustomRangeEnumerator_ForEachRange_Loops() {
+        var count = 0;
+        foreach (var i in 0..10) {
+            i.Should().Be(count++);
+        }
+        count.Should().Be(11);
+    }
+
+    [Fact]
+    public void CustomRangeEnumerator_ForEachRange_WithoutEnd_Throws() {
+        var action = () => { foreach (var _ in 0..); };
+
+        action.Should().Throw<NotSupportedException>().WithMessage("The range end must be provided.");
+    }
+}
---
+++ Sandbox.System/Extensions/CustomRangeEnumerator.cs
@@ -0,0 +1,22 @@
+﻿namespace System;
+
+public static class RangeExtensions {
+    public static CustomRangeEnumerator GetEnumerator(this Range range) => new(range);
+}
+
+public ref struct CustomRangeEnumerator {
+    private readonly int _end;
+
+    public CustomRangeEnumerator(Range range) {
+        if (range.End.IsFromEnd) throw new NotSupportedException("The range end must be provided.");
+        Current = range.Start.Value - 1;
+        _end = range.End.Value;
+    }
+
+    public int Current { get; private set; }
+
+    public bool MoveNext() {
+        Current++;
+        return Current <= _end;
+    }
+}
